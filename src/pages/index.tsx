12d68b124a6f922cb2b606c8c928c1a1d8cea256
yLost <fernandopovinsky@hotmail.com>
2022-04-15 19:22:25 -0300

    Feat: dynamic sockets

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
-import { useTheme, Link, Button, Loading } from "@wipsie/ui";
+import {
+  useTheme,
+  Link,
+  Button,
+  Loading,
+  Flex,
+  Container,
+  Typography,
+  Input,
+  Spacing,
+} from "@wipsie/ui";
 import DefaultLayout from "../components/DefaultLayout";
 import NextLink from "next/link";
 import { isProd } from "../config";
@@ -12,90 +22,134 @@ import axios from "axios";
 import io from "socket.io-client";
 
 export default function Home(props) {
-  const theme = useTheme();
-  const { numberOfCakes } = useSelector((state: any) => state.cake);
-  const { users, loading } = useSelector((state: any) => state.user);
-  const { posts } = useSelector((state: any) => state.blog);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchUsers());
-    dispatch(fetchPosts());
-  }, []);
+  const [accessToken, setAccessToken] = useState(null);
+  const [roomId, setRoomId] = useState(null);
+  const [listenEvent, setListenEvent] = useState([]);
+  const [newEvent, setNewEvent] = useState("");
+  const [messages, setMessages] = useState([]);
 
   // Socket setup start
   const [socket, setSocket] = useState(null);
 
-  const EVENTS = [
-    "NEW_FOLLOWER_PROJECT",
-    "NEW_FOLLOWER_USER",
-    "NEW_POST_PROJECT",
-    "NEW_POST_USER",
-    "NEW_COMMENT",
-  ];
-
-  useEffect(() => {
-    async function fetchData() {
-      const authApi = axios.create({
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      });
-
-      if (socket != null) {
-        socket.disconnect();
-      }
-
-      // const currentSessionRequest = await authApi.post(`/api/auth/session`);
-      // const currentSession = currentSessionRequest.data;
-
-      const _socket = io("https://env.dev.api.wipsie.com", {
-        path: "/sockets/socket.io/",
-        query: {
-          pageRefId: "editor01",
-          token: "",
-        },
-      });
-
-      _socket.on("NEW_FOLLOWER_PROJECT", (data) => {
-        console.log(data);
-      });
-
-      console.log(_socket);
-
-      setSocket(_socket);
+  async function connect() {
+    if (socket != null) {
+      socket.disconnect();
     }
 
-    if (!socket) {
-      fetchData();
+    const authApi = axios.create({
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+    });
+
+    if (socket != null) {
+      socket.disconnect();
     }
 
-    return () => {
-      if (socket != null) {
-        socket.off("NEW_FOLLOWER_PROJECT");
+    // const currentSessionRequest = await authApi.post(`/api/auth/session`);
+    // const currentSession = currentSessionRequest.data;
 
-        socket.disconnect();
-      }
-    };
-  }, [socket]);
-  // Socket setup end
+    const _socket = io("https://env.dev.api.wipsie.com", {
+      path: "/sockets/socket.io/",
+      query: {
+        pageRefId: roomId,
+        token: accessToken,
+      },
+    });
+
+    setSocket(_socket);
+  }
 
   return (
-    <DefaultLayout meta={{}}>
-      <NextLink href="/dois">
-        <Link>Dois</Link>
-      </NextLink>
-      <Button onClick={() => dispatch(buyCake())}>num: {numberOfCakes}</Button>
-      {loading ? (
-        <Loading />
-      ) : (
-        users.map((user: any) => <p key={user.id}>{user.name}</p>)
-      )}
+    <>
+      <Container m={2}>
+        <Typography variant="h1">Access Token </Typography>
+        <Input
+          value={accessToken}
+          onChange={(event: any) => setAccessToken(event.target.value)}
+        ></Input>
 
-      {posts.map((post: any) => (
-        <p key={post.id}>{post.title}</p>
-      ))}
-    </DefaultLayout>
+        <Spacing height={2} />
+
+        <Typography variant="h1">Room ID</Typography>
+        <Input
+          value={roomId}
+          onChange={(event: any) => setRoomId(event.target.value)}
+        ></Input>
+
+        <Spacing height={2} />
+
+        {socket != null ? (
+          <>
+            <Typography variant="h1">New Event</Typography>
+            <Input
+              value={newEvent}
+              onChange={(event: any) => setNewEvent(event.target.value)}
+            ></Input>
+            <Button
+              onClick={() => {
+                const _newEvent = newEvent;
+                setNewEvent("");
+
+                socket.on(_newEvent, (data) =>
+                  setMessages((messages) => [
+                    ...messages,
+                    { event: _newEvent, data },
+                  ])
+                );
+
+                setListenEvent((listenEvent) => {
+                  return [...listenEvent, _newEvent];
+                });
+              }}
+            >
+              Add
+            </Button>
+
+            {listenEvent.map((value, index) => {
+              return (
+                <Container>
+                  <Typography key={index} variant="h3">
+                    {value}
+                  </Typography>
+                  <Button
+                    onClick={() => {
+                      if (socket) {
+                        socket.off(value);
+
+                        setListenEvent((listenEvent) => {
+                          return listenEvent.filter((item) => item !== value);
+                        });
+                      }
+                    }}
+                    backgroundColor="danger"
+                  >
+                    Remove
+                  </Button>
+                </Container>
+              );
+            })}
+            <Spacing height={2} />
+          </>
+        ) : null}
+
+        <Button onClick={() => connect()}>Conectar</Button>
+      </Container>
+      <Container m={2}>
+        <Typography variant="h2">Messages</Typography>
+        <Spacing height={3} />
+        {messages.map((message, index) => {
+          return (
+            <>
+              <Typography key={index} variant="h3">
+                {message.event} - {JSON.stringify(message.data)}
+              </Typography>
+              <Spacing height={3} />
+            </>
+          );
+        })}
+      </Container>
+    </>
   );
 }
